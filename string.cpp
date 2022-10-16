9a42909bbf9093a95d6a0cceef9ff49404f5fe78
Guotong Liao <815190191@qq.com>
2022-10-16 10:58:46 -0700

    docs: update

--- string.cpp
+++ string.cpp
@@ -26,3 +26,31 @@ int main() {
         h[i] = h[i - 1] * P + s[i];  // if didn't add ' ' at front, will be s[i - 1]
     }
 }
+
+/*
+    Reverse each word in a string
+    "The sky is blue" -> "blue is sky The"
+
+    1. reverse it -> "eulb si yks ehT"
+    2. reverse each word -> "blue is sky The"
+
+    need to trim white space
+*/
+int reverseWord(string s) {
+    int k = 0;
+    for (int i = 0; i < s.size();)
+    {
+        int j = i;
+        while (j < s.size() && s[j] == ' ') j ++ ;
+        if (j == s.size()) break;
+        i = j;
+        while (j < s.size() && s[j] != ' ') j ++ ;
+        reverse(s.begin() + i, s.begin() + j);
+        if (k) s[k ++ ] = ' ';
+        while (i < j) s[k ++ ] = s[i ++ ];
+    }
+    s.erase(s.begin() + k, s.end());
+    reverse(s.begin(), s.end());
+
+    return s;
+}
